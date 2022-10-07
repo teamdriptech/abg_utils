9c97961d60ddd11157358a3c3d8a251778eea306
Hemantharajan <hemantharajan.v1@mrcooper.com>
2022-10-07 14:51:51 +0530

    remove stripe

--- lib/payments_web.dart
+++ lib/payments_web.dart
@@ -2,4 +2,3 @@ export 'src/payments/flutterwave_web.dart';
 export 'src/payments/razorpay_web.dart';
 export 'src/payments/mercado_pago_web.dart';
 export 'src/payments/mercado_pago_web2.dart';
-export 'src/payments/stripe_web.dart';
